38c1fc55a425cdd115534172799905463a31cd72
M-Enes <karaca.1453.27@gmail.com>
2024-01-04 00:56:43 +0300

    config system updated

--- src/main.c
+++ src/main.c
@@ -13,10 +13,13 @@ int col;
 int row;
 
 int main(){
+
+	// Reading Configuration
+	ConfigurationField* config_list = calloc(CONFIG_COUNT, sizeof(ConfigurationField));
+	read_config(config_list);
+
     signal(SIGWINCH,&resizeSignal);
 
-	// Reading Configuration File
-    read_config();
 
     // Getting Window Size
     struct winsize w;
@@ -24,7 +27,7 @@ int main(){
     col = w.ws_col;
     row = w.ws_row;
 
-    //Default Window Size
+    // Default Window Size
     if(col < DEFAULT_COLUMN){
         col = DEFAULT_COLUMN;
     }
@@ -44,7 +47,7 @@ int main(){
     }
     charBuffer[row] = NULL;
 
-    //Foreground Color Buffer Definition
+    // Foreground Color Buffer Definition
     int **FGColorBuffer;
     FGColorBuffer = (int **)calloc(row + 1, sizeof(int *));
     for (int i = 0; i < row; i++) {
@@ -57,7 +60,7 @@ int main(){
     }
     FGColorBuffer[row] = NULL;
 
-    //Background Color Buffer Definition
+    // Background Color Buffer Definition
     int **BGColorBuffer; BGColorBuffer = (int **)calloc(row+1,sizeof(int *));
     for(int i=0;i < row;i++){
         int *line = (int *)calloc(col+1,sizeof(int *));
@@ -70,28 +73,46 @@ int main(){
     BGColorBuffer[row] = NULL;
 
 
-    //Turning echoing off in stdin
+    // Turning echoing off in stdin
     struct termios term;
     tcgetattr(1, &term);
-    term.c_lflag &= -ECHO;//Blocking the echo of normal characters
-    term.c_lflag &= -ECHOCTL;//Blocking the echo of control characters
-    term.c_lflag &= -ICANON;//Blocking Canonical Mode
+    term.c_lflag &= -ECHO;// Blocking the echo of normal characters
+    term.c_lflag &= -ECHOCTL;// Blocking the echo of control characters
+    term.c_lflag &= -ICANON;// Blocking Canonical Mode
     tcsetattr(1, TCSANOW, &term);
 
     runScreen(charBuffer,FGColorBuffer,BGColorBuffer);
 
-    //Clearing Buffers
+    // Clearing Buffers
     free(charBuffer);
     free(FGColorBuffer);
     free(BGColorBuffer);
-
-    //Turning echoing on in stdin
+    
+	
+    // Turning echoing on in stdin
     term.c_lflag |= ECHO;
     term.c_lflag |= ECHOCTL;
     term.c_lflag |= ICANON;
     tcsetattr(1, TCSANOW, &term);
 
     system("clear");
+    
+
+    // Test stuff
+    
+    printf("Name: %s\n", get_config_field("BACKGROUND_COLOR", config_list).name);
+	printf("Value (int): %d\n", get_config_field("BACKGROUND_COLOR", config_list).value.int_value);
+	
+	printf("Name: %s\n", get_config_field("LINE_WRAPPING", config_list).name);
+	printf("Value (bool): %s\n", get_config_field("LINE_WRAPPING", config_list).value.bool_value ? "true" : "false");
+    
+    printf("Name: %s\n", get_config_field("CHAR_TEST", config_list).name);
+	printf("Value (char): %c\n", get_config_field("CHAR_TEST", config_list).value.char_value);
+    
+    printf("Name: %s\n", get_config_field("STRING_TEST", config_list).name);
+	printf("Value (string): %s\n", get_config_field("STRING_TEST", config_list).value.string_value);
+    
+    
     return 0;
 }
 
